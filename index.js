7962eb60a0846209aba59f2961a07daf77ab2886
Akash Mishra <leo.akashmishra@gmail.com>
2024-07-12 09:28:43 +0530

    This is final commit of the server

---
+++ index.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const express = require("express")
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("This is home page api")
+})
+
+app.get("/login",(req,res)=>{
+    res.send("This is login page api")
+})
+
+app.get("/register",(req,res)=>{
+    res.send("This is register page api")
+})
+
+app.get("/",(req,res)=>{
+    res.send("This is home page api")
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log('====================================');
+    console.log("Server is running on PORT : 8080");
+    console.log('====================================');
+})
